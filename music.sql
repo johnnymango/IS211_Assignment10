c8d658ab0c02bff676e0f57bb4b64f5c3fd01372
johnnymango <johnnydrodriguez@gmail.com>
2017-11-19 21:26:24 -0500

    music sql

---
+++ music.sql
@@ -0,0 +1,34 @@
+-- IS211 Assignment 10
+
+
+-- Deletes Tables if they exist
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS songs;
+
+
+-- Creates the artists table
+CREATE TABLE artists 
+(
+	artist_id int PRIMARY KEY, 
+	artist_name TEXT
+);
+
+-- Creates albums table
+CREATE TABLE albums 
+(
+	album_id int PRIMARY KEY, 
+	album_name text,	
+    artist_id int
+);
+
+-- Creates songs table
+CREATE TABLE songs 
+(
+	song_id int PRIMARY KEY, 
+	song_name text,
+	artist_id int,
+    album_id int, 
+	track_number int, 
+	song_length int
+);
